60852770fe68a8de1d4e88e8b7bcbd5ba77625be
Joseph Rohani <britishperson10@protonmail.com>
2022-06-14 13:01:35 +0100

    Redirects to www version now

---
+++ redirect.js
@@ -0,0 +1,9 @@
+if (window.location.href=="file:///home/joseph/code/website/alt/index.html"){
+window.location.href="https://www.josephrohani.ch/";
+}
+        if (window.location.href=="http://josephrohani.ch/"){
+            window.location.href="https://www.josephrohani.ch/";
+        }
+            if (window.location.href=="https://josephrohani.ch/"){
+            window.location.href="https://www.josephrohani.ch/";
+        }
